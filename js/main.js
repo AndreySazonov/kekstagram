aab3dfcfea63266baa81b8a69552e1a20d80a3af
Andrey <sazonov31294@gmail.com>
2023-06-08 15:41:41 +0300

    Правка функций для случайного целого числа и проверки максимальной длины строки

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@
 function getRandomInteger(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  if (min >= 0 && max >= 0 && max > min) {
+  if (min >= 0 && max >= 0 && max > min && min !== String && max !== String) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+  return 'Исходные данные неверны';
 }
 
 //Функция для проверки максимальной длины строки
