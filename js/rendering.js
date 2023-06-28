740d38c5be118dcd6404c35ab3fe021aa6dde4b9
Andrey <sazonov31294@gmail.com>
2023-06-28 15:56:59 +0300

    Деструктуризация параметров

--- js/rendering.js
+++ js/rendering.js
@@ -3,18 +3,18 @@ import {createNewPosts} from './data.js';
 //Контейнер для отрисовки фотографий
 const picturesList = document.querySelector('.pictures');
 
-//Шаблон фотографии
+//Шаблон поста с фотографией
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
 const pictureNewPost = createNewPosts();
 
 const pictureListFragment = document.createDocumentFragment();
 
-pictureNewPost.forEach((post) => {
+pictureNewPost.forEach(({url, likes, comments}) => {
   const pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__img').src = post.url;
-  pictureElement.querySelector('.picture__likes').textContent = post.likes;
-  pictureElement.querySelector('.picture__comments').textContent = post.comments.length;
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
   pictureListFragment.append(pictureElement);
 });
 
